139fa077363a19911daa70da97962f4137744e01
Tatsuya Sugawara <tatsuya.sugawara.sys@gmail.com>
2017-09-12 11:56:58 +0900

    基本構成 #1

---
+++ src/HoLMain.java
@@ -0,0 +1,42 @@
+import java.util.Random;
+import java.util.Scanner;
+
+public class HoLMain{
+	final static int eight = 8;
+	public static void main(String[] args){
+		boolean end = false;
+		Random rnd = new Random();
+		Scanner sc = new Scanner(System.in);
+		int x;
+		while(end == false){
+			int card = rnd.nextInt(12) + 1;
+			System.out.print("High=1 Low=2：");
+			x = sc.nextInt();
+			System.out.println("数値は"+card);
+			if(x == 1){
+				if(card >= eight){
+					System.out.println("正解");
+				}
+				else{
+					System.out.println("不正解");
+				}
+			}
+			else if(x == 2){
+				if(card < eight){
+					System.out.println("正解");
+				}
+				else{
+					System.out.println("不正解");
+				}
+			}
+			else{
+				System.out.println("1か2を入力してください");
+			}
+			System.out.print("続ける=1 終わる=1以外の数字");
+			int check = sc.nextInt();
+			if(check != 1){
+				end = true;
+			}
+		}
+	}
+}
